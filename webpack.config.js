6b9a4c809250be9925983b3c391e6a070224a6c2
Alfred Wei <Alfred_Wei@epam.com>
2017-11-23 18:29:23 +0800

    env works

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    path.resolve(__dirname, 'src/app/index.js'),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    loaders: [{
+      test: /\.js[x]?$/,
+      include: path.resolve(__dirname, 'src'),
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015'],
+      },
+    }]
+  }
+}
