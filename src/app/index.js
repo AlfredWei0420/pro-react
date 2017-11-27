4b1a8b3ed2ef37bc2381c55f20dea39e7cf7e0b4
Alfred Wei <Alfred_Wei@epam.com>
2017-11-27 14:33:42 +0800

    eslint reconfig

--- src/app/index.js
+++ src/app/index.js
@@ -2,12 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GroceryList from './GroceryList.jsx';
 
-class Hello extends React.Component {
-  render() {
-    return (
-      <h1>Hello React</h1>
-    );
-  }
-}
 
 ReactDOM.render(<GroceryList />, document.getElementById('root'));
