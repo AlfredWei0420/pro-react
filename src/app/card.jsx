5a6a1d2562d21d680e61305c7217fc61e49e0bba
Alfred Wei <Alfred_Wei@epam.com>
2017-11-28 17:57:01 +0800

    add click

--- src/app/card.jsx
+++ src/app/card.jsx
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import CheckList from './checkList.jsx';
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDetails: false,
+    };
+  }
+  changeState() {
+    this.setState({showDetails: !this.state.showDetails});
+    console.log(this.state.showDetails);
+  }
   render() {
+    let cardDetails;
+    if (this.state.showDetails) {
+      cardDetails = (
+        <div className="card_details">
+          {this.props.description}
+          <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+        </div>
+      );
+    }
+    console.log(cardDetails);
     return (
       <div className="card">
-      <div className="card_title">{this.props.title}</div>
-      <div className="card_details">
-        {this.props.description}
-        <CheckList cardId={this.props.id} tasks={this.props.tasks} />
-      </div>
+      <div className="card_title" onClick={this.changeState.bind(this)}>{this.props.title}</div>
+      {cardDetails}
     </div>
     )
   }
