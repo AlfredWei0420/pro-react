80a72429f17bd06fdcc48adb79aae942c8567d27
Alfred Wei <Alfred_Wei@epam.com>
2017-12-01 17:36:33 +0800

    add api

---
+++ src/app/KanbanBoardContainer.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import KanbanBoard from './board.jsx';
+
+const API_URL = 'http://kanbanapi.pro-react.com';
+const API_HEADERS = {
+  'Content-Type': 'application/json',
+   'Authorization': 'any-string-you-like'
+};
+
+class KanbanBoardContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      cards: [],
+    }
+  }
+  componentDidMount() {
+    let that = this;
+    fetch(API_URL+'/cards', {
+      method: 'GET',
+      headers: API_HEADERS,
+    }).then(function(response) {
+      response.json().then(function(responseData) {
+        that.setState({cards: responseData})
+      }).catch(function(error) {
+        console.log('Error fetching and parsing data', error)
+        })
+    })
+  }
+
+  render() {
+    return <KanbanBoard cards={this.state.cards} />
+  }
+}
+export default KanbanBoardContainer;
--- src/app/index.js
+++ src/app/index.js
@@ -2,43 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Search from './controlled.jsx';
 import Form from './uncontrolled.jsx';
-import KanbanBoard from './board.jsx';
+import KanbanBoardContainer from './KanbanBoardContainer.jsx';
 import ContactsApp from './contact.jsx';
 
-let cardsList = [
-  {
-    id: 1,
-    title: "Read the Book",
-    description: "I should read the whole book",
-    color:"#BD8D31",
-    status: "in-progress",
-    tasks: []
-  },
-  {
-    id: 2,
-    title: "Write some code",
-    description: "Code along with the samples in the book",
-    color:"#3A7E28",
-    status: "todo",
-    tasks: [
-      {
-        id: 1,
-        name: "ContactList Example",
-        done: true
-      },
-      {
-        id: 2,
-        name: "Kanban Example",
-        done: false
-      },
-      {
-        id: 3,
-        name: "My own experiments",
-        done: false
-      }
-    ]
-  }
-];
+// let cardsList = [
+//   {
+//     id: 1,
+//     title: "Read the Book",
+//     description: "I should read the whole book",
+//     color:"#BD8D31",
+//     status: "in-progress",
+//     tasks: []
+//   },
+//   {
+//     id: 2,
+//     title: "Write some code",
+//     description: "Code along with the samples in the book",
+//     color:"#3A7E28",
+//     status: "todo",
+//     tasks: [
+//       {
+//         id: 1,
+//         name: "ContactList Example",
+//         done: true
+//       },
+//       {
+//         id: 2,
+//         name: "Kanban Example",
+//         done: false
+//       },
+//       {
+//         id: 3,
+//         name: "My own experiments",
+//         done: false
+//       }
+//     ]
+//   }
+// ];
 
 let contacts = [
   {name: "Cassio Zen", email: "cassionzen@gamil.com"},
@@ -49,5 +49,5 @@ let contacts = [
   {name: "Sebasian Markbage", email: "semarkbage@gamil.com"},
 ];
 
-// ReactDOM.render(<KanbanBoard cards={cardsList}  />, document.getElementById('root'));
-ReactDOM.render(<ContactsApp contacts={contacts} />, document.getElementById('root'));
+ReactDOM.render(<KanbanBoardContainer />, document.getElementById('root'));
+// ReactDOM.render(<ContactsApp contacts={contacts} />, document.getElementById('root'));
